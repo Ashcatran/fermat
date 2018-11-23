000378a931db7013b0e74f1ac588d3cd7de890ce
Nicolas Donnaint <ashcatran@gmail.com>
2018-11-23 19:06:49 +0100

    Added 2 ways for user input

--- fermat.py
+++ fermat.py
@@ -3,6 +3,9 @@
 
 import math
 
+import sys
+
+#Pseudo Code from https://fr.wikipedia.org/wiki/M%C3%A9thode_de_factorisation_de_Fermat
 #FactorFermat(N): // N doit être impair
 #A ← arrondi(sqrt(N))
 #Bsq ← A*A - N
@@ -22,21 +25,37 @@ def isSquare(number):
 def isOdd(number):
     return False if number % 2 == 0 else True
 
-if __name__ == '__main__':
-
-    number =8000009 * 97 #3000017 * 3999971 #1933 * 2347 #83*97
+def fermat(number):
     if isOdd(number):
-        #print(number,"is odd")
         a = math.floor(math.sqrt(number))
-        #print(a)
         bsq = a*a - number
-        #print(bsq)
         while not isSquare(bsq):
             a +=1
             bsq = a*a - number
         result = a - math.sqrt(bsq)
-        print(number, "=", result, "*", number/result)
+        return int(result)
+
 
     else:
-        print(number, "is not odd")
+        return -1
 
+if __name__ == '__main__':
+    #8000009 * 97 #3000017 * 3999971 #1933 * 2347 #83*97
+    print(sys.version_info)
+    if len(sys.argv) == 2:
+        user_in = int(sys.argv[1])
+    else:
+        user_in = input("Please enter the number you wish to factorize: ")
+
+    if type(user_in) is not 'int':
+        print("You must specify an integer")
+        exit()
+    
+    number = int(user_in)
+    result = fermat(number)
+    if result == -1 :
+        print(str(number) + " is not odd")
+    elif result == 1:
+        print(str(number) + " is prime !")
+    else:    
+        print(str(number) + " = " + str(result) + " * " + str(int(number/result)))
